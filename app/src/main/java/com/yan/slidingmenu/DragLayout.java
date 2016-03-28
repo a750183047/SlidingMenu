b15df00578a760e0e91a0e15cb003c4be2dc5a8d
a750183047 <750183047@qq.com>
2016-03-28 16:04:27 +0800

    重写回调方法

--- app/src/main/java/com/yan/slidingmenu/DragLayout.java
+++ app/src/main/java/com/yan/slidingmenu/DragLayout.java
@@ -18,6 +18,9 @@ public class DragLayout extends FrameLayout {
     private ViewGroup mMainContent;
 
     private ViewDragHelper mDragHelper;
+    private int mWidth;
+    private int mHight;
+    private int mRange;
 
     public DragLayout(Context context) {
         this(context, null);
@@ -48,10 +51,39 @@ public class DragLayout extends FrameLayout {
             return true;
         }
 
+        @Override
+        public void onViewCaptured(View capturedChild, int activePointerId) {
+            //当 capturedChild被捕获时 调用
+            super.onViewCaptured(capturedChild, activePointerId);
+
+        }
+
+        @Override
+        public int getViewHorizontalDragRange(View child) {
+
+            //返回拖拽的范围 不对拖拽进行真正的限制 仅仅决定了动画执行速度
+            return mRange;
+        }
+
         //2 根据建议值修正要移动（横向）的位置
+        //此时没有发生真正的移动
         @Override
         public int clampViewPositionHorizontal(View child, int left, int dx) {
+            // child  当前拖拽的 View
+            //left  新的位置的建议值  dx 位置变化量
             return left;
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return super.clampViewPositionVertical(child, top, dy);
+        }
+
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            super.onViewPositionChanged(changedView, left, top, dx, dy);
+            //为了兼容低版本，每次修改值之后  进行重绘
+            invalidate();
         }
     };
     //2 传递触摸事件
@@ -85,5 +117,16 @@ public class DragLayout extends FrameLayout {
         mLiftContent = (ViewGroup) getChildAt(0);
         mMainContent = (ViewGroup) getChildAt(1);
 
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        //当尺寸有变化的时候调用
+
+        mHight = getMeasuredHeight();
+        mWidth = getMeasuredWidth();
+
+        mRange = (int) (0.6 *mWidth);
     }
 }
